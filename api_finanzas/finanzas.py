c3fc2973080c275bb3eb8fa9a5aac3913b42d33a
Osvaldo Valdivia <ovaldovia983@gmail.com>
2026-05-31 14:59:45 -0600

    Tarjeta Finanzas Y Campo Fecha en api finanzas

--- api_finanzas/finanzas.py
+++ api_finanzas/finanzas.py
@@ -1,8 +1,18 @@
 import requests
+from datetime import datetime
 
 BASE_URL = "https://mx.dolarapi.com/v1/cotizaciones"
 MONEDA = "usd"
 
+def get_current_date_formatted() -> str:
+    ahora = datetime.now()
+    periodo = "a.m" if ahora.hour < 12 else "p.m"
+    hora_12 = ahora.hour if ahora.hour <= 12 else ahora.hour - 12
+    if hora_12 == 0:
+        hora_12 = 12
+    
+    return f"{ahora.day}/{ahora.month}/{ahora.year}, {hora_12}:{ahora.minute:02d}:{ahora.second:02d} {periodo}"
+    
 def get_stock_price():
     try:
         url = f"{BASE_URL}/{MONEDA}"
@@ -19,7 +29,8 @@ def get_stock_price_description(data: dict) -> dict:
     return {
         "moneda":   data["moneda"],
         "nombre":   data["nombre"],
-        "compra":   data["compra"],
+        "compra":   round(data["compra"], 2),
+        "fecha": get_current_date_formatted(),
         "error":    None
     }
 
